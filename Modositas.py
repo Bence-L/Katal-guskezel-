4b668faee5a020f3cc18cf3076c3e7f989f07a09
Bence-L <11c-leber@ipari.vein.hu>
2024-11-15 13:52:42 +0100

    Megnyitások

---
+++ Modositas.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+def Mfuggveny():
+    Modositas = Tk()
+    Modositas.geometry("600x600")
+    Modositas.title("Módosítás")
+    Modositas.configure(bg="#B99470")
+    proba = Label(Modositas, text="Módosítás", bg="#B99470", fg="#664343",font=('sans', 60,'bold'),)
+    proba.grid(row= 0, columnspan=1,pady=(10,0), padx=200)
