009994165293a12c08c4806ede9a34d5e467c7bc
SkibidiCum_Poor <pobence2007@gmail.com>
2024-11-22 13:11:54 +0100

    kolcsonzott konyvek, konyvtablazat

---
+++ kolcsonzottkonyvek.py
@@ -0,0 +1,9 @@
+from tkinter import*
+import tkinter as tk
+from tkinter import ttk
+def kolcson():
+    kolcson = Tk()
+    kolcson.geometry("1000x800")
+    kolcson.title("Főoldal")
+    kolcson.configure(bg="black")
+    kolcson.mainloop()
---
+++ konyvtablazat.py
@@ -0,0 +1,11 @@
+from tkinter import*
+import tkinter as tk
+from tkinter import ttk
+def tablazat():
+    tablazat = Tk()
+    tablazat.geometry("1000x800")
+    tablazat.title("Főoldal")
+    tablazat.configure(bg="black")
+    tablazat.mainloop()
+
+    
