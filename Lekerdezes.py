823af12439205d92a99e70e9e4065aa75f5602c8
Bence-L <11c-leber@ipari.vein.hu>
2024-11-22 12:43:46 +0100

    Mukodik

--- Lekerdezes.py
+++ Lekerdezes.py
@@ -6,8 +6,4 @@ def Lfuggveny():
     Lekerdezes.title("Lekérdezés")
     Lekerdezes.configure(bg="#B99470")
     proba = Label(Lekerdezes, text="Lekérdezés", bg="#B99470", fg="#664343",font=('sans', 60,'bold'),)
-<<<<<<< HEAD
     proba.grid(row= 0, columnspan=1,pady=(10,10), padx=200)
-=======
-    proba.grid(row= 0, columnspan=1,pady=(10,0), padx=200)
->>>>>>> 96125ce68ac0ea1bc79b6fa1b3d50ad3f991c582
--- Torles.py
+++ Torles.py
@@ -6,8 +6,4 @@ def Tfuggveny():
     Torles.title("Törlés")
     Torles.configure(bg="#B99470")
     proba = Label(Torles, text="Törlés", bg="#B99470", fg="#664343",font=('sans', 60,'bold'),)
-<<<<<<< HEAD
     proba.grid(row= 0, columnspan=1,pady=(10,10), padx=200)
-=======
-    proba.grid(row= 0, columnspan=1,pady=(10,0), padx=200)
->>>>>>> 96125ce68ac0ea1bc79b6fa1b3d50ad3f991c582
