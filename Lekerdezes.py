ba49e97d81f7244567d0270218a28153d1f222c3
Bence-L <11c-leber@ipari.vein.hu>
2024-11-22 12:36:11 +0100

    Elveszik

---
+++ Lekerdezes.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+def Lfuggveny():
+    Lekerdezes = Tk()
+    Lekerdezes.geometry("600x600")
+    Lekerdezes.title("Lekérdezés")
+    Lekerdezes.configure(bg="#B99470")
+    proba = Label(Lekerdezes, text="Lekérdezés", bg="#B99470", fg="#664343",font=('sans', 60,'bold'),)
+    proba.grid(row= 0, columnspan=1,pady=(10,10), padx=200)
