c205680034ba2f604a1e6bc0254073f4fcfcf767
Bence-L <11c-leber@ipari.vein.hu>
2024-12-13 12:41:52 +0100

    Admin

--- Modositas.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-def Mfuggveny():
-    Modositas = Tk()
-    Modositas.geometry("600x600")
-    Modositas.title("Módosítás")
-    Modositas.configure(bg="#B99470")
-    proba = Label(Modositas, text="Módosítás", bg="#B99470", fg="#664343",font=('sans', 60,'bold'),)
-    proba.grid(row= 0, columnspan=1,pady=(10,10), padx=200)
---
+++ ujkonyv.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+def ujfuggveny():
+    ujkonyvek = tk.Tk()
+    ujkonyvek.geometry("1420x600")
+    ujkonyvek.title("Könyvek")
+    ujkonyvek.configure(bg="#9A7E6F")
