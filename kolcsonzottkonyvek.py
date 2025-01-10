9a2828ad030941a473bf1c6390b49b42bf422be1
SkibidiCum_Poor <pobence2007@gmail.com>
2025-01-10 11:42:14 +0100

    Pull

--- kolcsonzottkonyvek.py
+++ kolcsonzottkonyvek.py
@@ -1,9 +1,75 @@
 from tkinter import*
 import tkinter as tk
 from tkinter import ttk
+
+
+class Objektum:
+    def __init__(self, sorszam, cim, evszam, kiado, oldalszam, isbn, igennem):
+        self.sorszam = sorszam
+        self.cim = cim
+        self.evszam = evszam
+        self.kiado = kiado
+        self.oldalszam = oldalszam
+        self.isbn = isbn
+        self.igennem = igennem
+
 def kolcson():
     kolcson = Tk()
     kolcson.geometry("1000x800")
     kolcson.title("Főoldal")
     kolcson.configure(bg="black")
+    kolcson.mainloop()
+
+
+
+ 
+
+    proba = tk.Label(kolcson, text="Könyvek", bg="#9A7E6F", fg="#493628", font=('sans', 70, 'bold'))
+    proba.grid(row=0, columnspan=1, pady=(10, 10), padx=(450,100))
+
+    def beolvasas():
+        global books
+        books = []
+        with open('könyvek.txt', 'r', encoding='utf-8') as fajl:
+            for i, sor in enumerate(fajl, start=1):
+                adat = sor.strip().split(',')  
+                cim = adat[0].strip()  
+                evszam = adat[1].strip()
+                kiado = adat[2].strip()
+                oldalszam = adat[3].strip()
+                isbn = adat[4].strip()
+                igennem = adat[5].strip()
+                book = Objektum(i, cim, evszam, kiado, oldalszam, isbn, igennem)  
+                books.append(book)
+
+    def show():
+        for item in listBox.get_children():
+            listBox.delete(item)
+        
+        sorted_books = sorted(books, key=lambda book: book.sorszam) 
+
+        for i, book in enumerate(sorted_books, start=1):
+            if i % 2 == 0:
+                listBox.insert("", "end", values=(book.sorszam, book.cim, book.evszam, book.kiado, book.oldalszam, book.isbn, book.igennem),
+                               tags=('even'))
+            else:
+                listBox.insert("", "end", values=(book.sorszam, book.cim, book.evszam, book.kiado, book.oldalszam, book.isbn, book.igennem))
+
+        listBox.tag_configure('even', background='#AB886D') 
+
+    cols = ('Sorszám', 'Cím', 'Kiadási dátum', 'Kiadó', 'Oldalszám', 'ISBN', 'Kölcsönzött-e?')
+    listBox = ttk.Treeview(kolcson, columns=cols, show='headings')
+
+    for col in cols:
+        listBox.heading(col, text=col)
+
+    listBox.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+
+    showScores = tk.Button(kolcson, text="Adatok mutatása",  fg="#493628", bg="#D6C0B3", font="sans 13 bold", command=show)
+    showScores.grid(row=4, column=0, padx=(480, 100))
+
+    closeButton = tk.Button(kolcson, text="Vissza", fg="#493628", bg="#FF8A8A", font="sans 13 bold", command=kolcson.destroy)
+    closeButton.grid(row=5, column=0 , pady=50, padx=(480, 100))
+
+    beolvasas()
     kolcson.mainloop()
