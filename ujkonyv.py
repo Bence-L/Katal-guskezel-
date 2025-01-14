f644211c5e4131e8316fc37ebeb95abda20b64e0
lbrbence169 <leber.bence08@gmail.com>
2025-01-14 18:18:39 +0100

    Ujkönyv kommentezése

--- ujkonyv.py
+++ ujkonyv.py
@@ -12,15 +12,18 @@ class Objektum:
         self.oldalszam = oldalszam
         self.isbn = isbn
         self.igennem = igennem
+
 def ujfuggveny():
     ujkonyvek = tk.Tk()
     ujkonyvek.geometry("750x1300")
     ujkonyvek.title("Könyvek📖")
     ujkonyvek.configure(bg="#9A7E6F")
-
-    proba = Label(ujkonyvek, text="Hozzáadás", bg="#9A7E6F", fg="#493628", font=('sans', 50, 'bold'))
+    
+    #Címsor
+    proba = Label(ujkonyvek, text="Hozzáadás", bg="#9A7E6F", fg="#493628", font=('sans', 40, 'bold'))
     proba.grid(row=0, column=0, columnspan=2, pady=(10, 10), sticky="nsew")
 
+    #Beolvasás
     def beolvasas():
         global books
         books = []
@@ -36,12 +39,14 @@ def ujfuggveny():
                 book = Objektum(i, cim, evszam, kiado, oldalszam, isbn, igennem)
                 books.append(book)
 
+    #Mentés
     def mentes():
         with open('könyvek.txt', 'w', encoding='utf-8') as fajl:
             for book in books:
                 fajl.write(f"{book.cim},{book.evszam},{book.kiado},{book.oldalszam},{book.isbn},{book.igennem}\n")
 
-    def show():
+    #Adatok megjelenítése a táblázatban
+    def megjelenit():
         for item in listBox.get_children():
             listBox.delete(item)
 
@@ -53,6 +58,7 @@ def ujfuggveny():
                 listBox.insert("", "end", values=(book.sorszam, book.cim, book.evszam, book.kiado, book.oldalszam, book.isbn, book.igennem))
         listBox.tag_configure('even', background='#AB886D')
 
+    #Az új könyv hozzáadása a táblázathoz és a txthez is.
     def hozzaadas():
         # Mezők értékeinek kiolvasása
         cim = Cím.get().strip()
@@ -61,34 +67,34 @@ def ujfuggveny():
         oldalszam = oldalszam_mezo.get().strip()
         isbn = isbn_mezo.get().strip()
 
-        # Mezők listája
+        #A mezők listája
         mezok = [cim, evszam, kiado, oldalszam, isbn]
         
-        # Üres mezők ellenőrzése
+        #Megnézi hogy van e üres mező
         ures_mezo_talalva = False
         for mezo in mezok:
             if mezo == "":
                 ures_mezo_talalva = True
                 break
 
-        # Ha találtunk üres mezőt, figyelmeztetés
+        # Ha talál üres mezőt, felugrik egy üzenet ami figyelmeztet
         if ures_mezo_talalva:
             messagebox.showwarning("Hiányzó adatok", "Kérlek tölts ki minden mezőt!")
             return
 
-        # Új objektum létrehozása
+        #Új objektum készítése
         sorszam = len(books) + 1
         uj_konyv = Objektum(sorszam, cim, evszam, kiado, oldalszam, isbn, "Nem")
         books.append(uj_konyv)
 
-        # Táblázat frissítése
+        #Táblázat frissítése
         listBox.insert("", "end", values=(sorszam, cim, evszam, kiado, oldalszam, isbn, "Nem"))
 
-        # Fájl frissítése
+        #könyvek.txt frissítése
         with open('könyvek.txt', 'a', encoding='utf-8') as fajl:
             fajl.write(f"{cim},{evszam},{kiado},{oldalszam},{isbn},Nem\n")
 
-        # Mezők ürítése
+        #Mezők ürítése a beküldés után hogy megint lehessen hozzáadni
         Cím.delete(0, tk.END)
         datum.delete(0, tk.END)
         kiado_mezo.delete(0, tk.END)
@@ -100,12 +106,14 @@ def ujfuggveny():
     for col in cols:
         listBox.heading(col, text=col)
         listBox.column(col, width=100)
-
+    
+    #Táblázathoz görgetés.
     gorgeto = ttk.Scrollbar(ujkonyvek, orient="vertical", command=listBox.yview)
     listBox.configure(yscroll=gorgeto.set)
     listBox.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
     gorgeto.grid(row=1, column=2, sticky="ns")
 
+    #gomb stílusai és elhelyezkedés helye
     gomb_keret = Frame(ujkonyvek, bg="#9A7E6F")
     gomb_keret.grid(row=4, column=0, columnspan=2, pady=10, sticky="nsew")
     kilepo_keret = Frame(ujkonyvek, bg="#9A7E6F")
@@ -113,49 +121,48 @@ def ujfuggveny():
     hozzaado_keret = Frame(ujkonyvek, bg="#9A7E6F")
     hozzaado_keret.grid(row=15, column=0, columnspan=2, pady=10, sticky="nsew")
 
-    show_button = tk.Button(gomb_keret, text="Adatok mutatása", fg="#493628", bg="#D6C0B3", font="sans 13 bold", 
-                            width=30, height=2, command=show)
-    show_button.pack(pady=5)
+    #gomb ami megjeleníti az adatokat
+    adatok_megjelenitese = tk.Button(gomb_keret, text="Adatok mutatása", fg="#493628", bg="#D6C0B3", font="sans 13 bold", width=30, height=2, command=megjelenit)
+    adatok_megjelenitese.pack(pady=3)
 
+    #Az entry mezők és a hozzájuk tartozó leírások
     cime = Label(ujkonyvek, text="Írja be a címet ", fg="#493628", bg="#9A7E6F", font=('Comic Sans', 10, 'bold'))
-    cime.grid(row=5, column=0, columnspan=2, pady=(5, 5), sticky="nsew")
+    cime.grid(row=5, column=0, columnspan=2, pady=(2, 2), sticky="nsew")
 
     Cím = Entry(ujkonyvek, width=20, bg="#D6C0B3")
-    Cím.grid(row=6, column=0, columnspan=2, pady=(0, 5))
+    Cím.grid(row=6, column=0, columnspan=2, pady=(0, 3))
 
     datuma = Label(ujkonyvek, text="Írja be a kiadasi dátumot:", fg="#493628", bg="#9A7E6F", font=('Comic Sans', 10, 'bold'))
-    datuma.grid(row=7, column=0, columnspan=2, pady=(5, 5), sticky="nsew")
+    datuma.grid(row=7, column=0, columnspan=2, pady=(2, 2), sticky="nsew")
 
     datum = Entry(ujkonyvek, width=20, bg="#D6C0B3")
-    datum.grid(row=8, column=0, columnspan=2, pady=(0, 5))
+    datum.grid(row=8, column=0, columnspan=2, pady=(0, 3))
 
     kiadoja = Label(ujkonyvek, text="Írja be a Kiadót: ", fg="#493628", bg="#9A7E6F", font=('Comic Sans', 10, 'bold'))
-    kiadoja.grid(row=9, column=0, columnspan=2, pady=(5, 5), sticky="nsew")
+    kiadoja.grid(row=9, column=0, columnspan=2, pady=(2, 2), sticky="nsew")
 
     kiado_mezo = Entry(ujkonyvek, width=20, bg="#D6C0B3")
-    kiado_mezo.grid(row=10, column=0, columnspan=2, pady=(0, 5))
+    kiado_mezo.grid(row=10, column=0, columnspan=2, pady=(0, 3))
 
     oldalszama = Label(ujkonyvek, text="Írja be az Oldalszámot: ", fg="#493628", bg="#9A7E6F", font=('Comic Sans', 10, 'bold'))
-    oldalszama.grid(row=11, column=0, columnspan=2, pady=(5, 5), sticky="nsew")
+    oldalszama.grid(row=11, column=0, columnspan=2, pady=(2, 2), sticky="nsew")
 
     oldalszam_mezo = Entry(ujkonyvek, width=20, bg="#D6C0B3")
-    oldalszam_mezo.grid(row=12, column=0, columnspan=2, pady=(0, 5))
+    oldalszam_mezo.grid(row=12, column=0, columnspan=2, pady=(0, 3))
 
     isbne = Label(ujkonyvek, text="Írja be az ISBN-t: ", fg="#493628", bg="#9A7E6F", font=('Comic Sans', 10, 'bold'))
-    isbne.grid(row=13, column=0, columnspan=2, pady=(5, 5), sticky="nsew")
+    isbne.grid(row=13, column=0, columnspan=2, pady=(2, 2), sticky="nsew")
 
     isbn_mezo = Entry(ujkonyvek, width=20, bg="#D6C0B3")
-    isbn_mezo.grid(row=14, column=0, columnspan=2, pady=(0, 5))
+    isbn_mezo.grid(row=14, column=0, columnspan=2, pady=(0, 3))
 
-    hozzaadas_button = tk.Button(hozzaado_keret, text="Hozzáadás", fg="#493628", bg="#D6C0B3", font="sans 13 bold", 
-                             width=5, height=2, command=hozzaadas)
+    #Hozzáadó gomb és a bezáró gomb
+    hozzaadas_button = tk.Button(hozzaado_keret, text="Hozzáadás", fg="#493628", bg="#D6C0B3", font="sans 13 bold", width=30, height=2, command=hozzaadas)
     hozzaadas_button.pack(pady=10)
 
-    close_button = tk.Button(kilepo_keret, text="Vissza", fg="#493628", bg="#FF8A8A", font="sans 13 bold", 
-                             width=30, height=2, command=ujkonyvek.destroy)
+    close_button = tk.Button(kilepo_keret, text="Vissza", fg="#493628", bg="#FF8A8A", font="sans 13 bold", width=30, height=2, command=ujkonyvek.destroy)
     close_button.pack(pady=(0,10))
 
     beolvasas()
     ujkonyvek.mainloop()
 
-ujfuggveny()
